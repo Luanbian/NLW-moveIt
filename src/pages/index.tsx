fe61874bb37d561b236ee57771e57d77c99a4430
acer linux <luanbianchine1@gmail.com>
2022-04-30 19:34:00 -0300

    beginnig of timer pomodoro completed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,23 @@
 import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/CountDown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Perfil";
 import styles from '../styles/pages/home.module.css';
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>move.it</title>
+      </Head>
       <ExperienceBar/>
 
       <section>
         <div>
           <Profile/>
           <CompletedChallenges/>
+          <Countdown/>
         </div>
         <div>
         
