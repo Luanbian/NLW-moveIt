4b448665c7587d828e3ebcebc92971effd8dab4c
acer linux <luanbianchine1@gmail.com>
2022-04-30 18:32:20 -0300

    fixing bugs with css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { CompletedChallenges } from "../components/CompletedChallenge";
+import { CompletedChallenges } from "../components/CompletedChallenges";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Perfil";
 import styles from '../styles/pages/home.module.css';
@@ -11,9 +11,10 @@ export default function Home() {
       <section>
         <div>
           <Profile/>
+          <CompletedChallenges/>
         </div>
         <div>
-          <CompletedChallenges/>
+        
         </div>
       </section>
     </div> 
